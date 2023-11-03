f4d784d916cae39f73b7c1378218b1320ea5969c
Phil Todisco <philtodiscoglass@gmail.com>
2023-11-02 21:02:49 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const name = document.getElementById('name').value
+  const email = document.getElementById('email'.value)
+  const message = document.getElementById('message').value
+
+  const data = { name, email, message}
+
+  post('https://dttc-api.herokuapp.com/email', data)
+  .then(function(response) {
+    console.log('Email sent')
+  })
+  .catch(function(error) {
+    console.error('Error sending email:', error)
+  })
+})
+
+
